b8a7ec3363744a3c237c5d8c5d9efc0493e33176
c-chae-eun-n <ce.hue125@gmail.com>
2024-05-14 11:47:03 +0900

    Modify App Component using Theme

---
+++ src/theme.js
@@ -0,0 +1,15 @@
+// theme.js
+
+// 1. import `extendTheme` function
+import { extendTheme } from '@chakra-ui/react'
+
+// 2. Add your color mode config
+const config = {
+  initialColorMode: 'light',
+  useSystemColorMode: true,        // 시스템 모드에 따라 자동설정 : true
+}
+
+// 3. extend the theme
+const theme = extendTheme({ config })
+
+export default theme
